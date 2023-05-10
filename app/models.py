cc5d5b2a5cf315ee14da7257c2207d68fba0e5a0
valduborova <valduborova@gmail.com>
2023-05-10 09:24:15 +0300

    Second commit

--- app/models.py
+++ app/models.py
@@ -1,5 +1,5 @@
 from sqlalchemy import Column, Integer, String, ARRAY, CheckConstraint
-from database import Base
+from .database import Base
 
 
 class Couriers(Base):
--- app/router.py
+++ app/router.py
@@ -1,14 +1,27 @@
 from fastapi import APIRouter, Request
 from starlette import status
 from pydantic import BaseModel
-# import json
+
+from .database import SessionLocal
+from app import models
+
 
 router = APIRouter()
+db = SessionLocal()
 
 
-class Item(BaseModel):
-    name: str
+class Courier(BaseModel):
+    courier_id: int
+    region: int
+    type: str
+    working_graphics: str
 
+class Order(BaseModel):
+    order_id: int
+    weight: int
+    region: int
+    time_delievery: str
+    price: int
 
 # @router.get(
 #     "/ping",
@@ -34,8 +47,19 @@ class Item(BaseModel):
     "/couriers",
     status_code=status.HTTP_200_OK
 )
-async def post_couriers(item: Item):
-    return item.name
+async def post_couriers(item: Courier):
+    entry = models.Couriers(courier_id=item.courier_id,
+                            region=item.region,
+                            type=item.type,
+                            working_graphics=item.working_graphics)
+    # try:
+    db.add(entry)
+    db.commit()
+    db.close()
+        # return redirect('/posts')
+    # except:
+    #     return 'При добавлении записи произошла ошибка'
+    # return item.name
 
 
 @router.get(
