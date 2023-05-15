5d920b70f5318c5bb61eb412631e2af4f3951e76
Ivan Chekanov <is.2003.01@gmail.com>
2023-05-15 14:05:18 +0300

    :whale: Made docker work

--- Dockerfile
+++ Dockerfile
@@ -3,7 +3,7 @@ FROM python:3.10-alpine3.17 as builder
 RUN python3 -m venv /app
 RUN /app/bin/pip install -U pip
 
-COPY requirements.txt /mnt/
+COPY ./requirements.txt /mnt/requirements.txt
 RUN /app/bin/pip install -Ur /mnt/requirements.txt
 
 FROM python:3.10-alpine3.17 as app
@@ -12,7 +12,5 @@ WORKDIR /app
 
 COPY --from=builder /app /app
 COPY . .
-
-EXPOSE 8000
 
 CMD /app/bin/uvicorn app.main:app --host=0.0.0.0 --port=8080
--- app/database.py
+++ app/database.py
@@ -1,14 +1,37 @@
 from sqlalchemy import create_engine, event
 from sqlalchemy.orm import sessionmaker
+from sqlalchemy.sql import text
+from sqlalchemy.exc import ProgrammingError
 
 from .models import Base, CourierTypes
 
-SQLALCHEMY_DATABASE_URL = 'postgresql://valeriaduborova@localhost:5432/lavka'
+# SQLALCHEMY_DATABASE_URL = 'postgresql://valeriaduborova@localhost:5432/lavka'
 # SQLALCHEMY_DATABASE_URL = 'postgresql://root:root@localhost:5432/lavka'
+
+
+PG_USER = 'postgres'
+PG_PASSWORD = 'password'
+PG_HOST = 'db'
+PG_PORT = '5432'
+PG_DATABASE = 'PostgreSQL 15.2'
+
+SQLALCHEMY_URL = f'postgresql://{PG_USER}:{PG_PASSWORD}@{PG_HOST}:{PG_PORT}'
+SQLALCHEMY_DATABASE_URL = f'{SQLALCHEMY_URL}/{PG_DATABASE}'
+
+
+with create_engine(f"{SQLALCHEMY_URL}/postgres").connect() as conn:
+    conn.execute(text('COMMIT'))
+    try:
+        conn.execute(text(f'CREATE DATABASE "{PG_DATABASE}"'))
+        conn.execute(text('COMMIT'))
+    except ProgrammingError:
+        pass
+
 
 engine = create_engine(SQLALCHEMY_DATABASE_URL)
 SessionLocal = sessionmaker(bind=engine)
 Base.metadata.create_all(engine)
+
 
 with SessionLocal() as session:
     if not session.query(CourierTypes).first():
